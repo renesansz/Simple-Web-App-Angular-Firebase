3e4eaec579111f2b5d86327a162fbcfba9527bba
Renemari Padillo <padillo.renemari@gmail.com>
2015-03-20 02:31:11 +0800

    * Added second part of the demo.

---
+++ 2/angular/controllers/todo/controller.js
@@ -0,0 +1,53 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module('todo-app.controllers')
+        .controller('TodoController', TodoController);
+
+    function TodoController ($scope) {
+
+        /**
+         * TODO STATUS:
+         * 0 - Not done
+         * 1 - Done
+         */
+
+        $scope.todoList = [
+            {
+                id: 1,
+                title: 'Learn HTML',
+                status: 0
+            },
+            {
+                id: 2,
+                title: 'Learn JavaScript',
+                status: 0
+            },
+            {
+                id: 3,
+                title: 'Sleep',
+                status: 1
+            }
+        ];
+
+        $scope.AddTodo = function() {
+            $scope.todoList.push({
+                id: $scope.todoList.length + 1,
+                title: $scope.newTodo,
+                status: 0
+            });
+
+            $scope.newTodo = '';
+        };
+
+        $scope.DeleteTodo = function(index) {
+
+            $scope.todoList.splice(index, 1);
+
+        };
+
+    }
+
+})();
